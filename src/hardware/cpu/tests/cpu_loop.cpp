082b7a74c391aacd4de74dedba0fa23c45c22d69
Oliver Strong <oliver.russell.strong@gmail.com>
2023-12-09 19:14:05 +1000

    Starting to get CI/CD in the project

---
+++ src/hardware/cpu/tests/cpu_loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cpu_65C816.hpp>
+
+
+int main(int argc, char **argv) {
+	CPU_65C816 cpu = CPU_65C816();
+	std::cout << "Hello world! testing testing!\n";
+
+	return 1;
+}
